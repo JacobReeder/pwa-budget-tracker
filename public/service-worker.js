4203ec2aba5d4894b3231f5f815f1502e4a01a79
JacobReeder <jacob.reeder1@gmail.com>
2022-06-11 10:02:04 -0700

    update

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,7 +7,15 @@ const FILES_TO_CACHE = [
     "/",
     "/css/styles.css",
     "/js/idb.js",
-    "/js/index.js"
+    "/js/index.js",
+    "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png"
 
 ];
 
@@ -40,7 +48,7 @@ self.addEventListener('activate', function (e) {
   );
 });
 
-self.addEventListener('fetch', function (e) {
+/*self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url)
     e.respondWith(
       caches.match(e.request).then(function (request) {
@@ -54,5 +62,45 @@ self.addEventListener('fetch', function (e) {
   
       })
     )
-  })
+  })*/
+
+  self.addEventListener("fetch", function(event) {
+    // cache all get requests to /api routes
+    if (event.request.url.includes("/api/")) {
+      event.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(event.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+    }
+  
+    event.respondWith(
+      fetch(event.request).catch(function() {
+        return caches.match(event.request).then(function(response) {
+          if (response) {
+            return response;
+          } else if (event.request.headers.get("accept").includes("text/html")) {
+            // return the cached home page for all requests for html pages
+            return caches.match("/");
+          }
+        });
+      })
+    );
+  });
+
+
 
